1af2a5bf40fd41c775350573ee78978436f9a778
Trckly <chotkiydanik@gmail.com>
2023-10-27 09:09:12 +0300

    Initial commit. Implemented basic UI.(#1)

---
+++ Source/AlgLab2/AlgLab2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlgLab2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALGLAB2_API AAlgLab2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
